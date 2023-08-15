6185fae5078ef403f166f50f902b7cd1c1675119
Usman-Marwat <us895907a2@gmail.com>
2023-08-15 18:32:10 +0500

    Build the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiRose } from 'react-icons/gi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { PiDotsNineBold } from 'react-icons/pi';
@@ -6,6 +6,11 @@ import { PiDotsNineBold } from 'react-icons/pi';
 import './Navbar.css';
 
 function Navbar() {
+	const [navbar, setNavbar] = useState('menu');
+
+	const showNavbar = () => setNavbar('menu showNavbar');
+	const removeNavbar = () => setNavbar('menu');
+
 	return (
 		<div className="navBar">
 			<div className="logoDiv">
@@ -13,18 +18,18 @@ function Navbar() {
 				<span>Raj Rose</span>
 			</div>
 
-			<div className="menu">
+			<div className={navbar}>
 				<ul>
 					<li className="navList">Destination</li>
 					<li className="navList">About Us</li>
 					<li className="navList">Testimonial</li>
 					<li className="navList">Gallery</li>
 				</ul>
-				<AiFillCloseCircle className="icon" />
+				<AiFillCloseCircle className="icon closeIcon" onClick={removeNavbar} />
 			</div>
 
 			<button className="btn signUpBtn">Sign Up</button>
-			<PiDotsNineBold className="icon" />
+			<PiDotsNineBold className="icon menuIcon" onClick={showNavbar} />
 		</div>
 	);
 }
