49a5e4c6ade38f3d635f9fc20e3730f896d3f1ec
Usman-Marwat <us895907a2@gmail.com>
2023-08-15 23:52:27 +0500

    Create accordian header for the questions

--- src/components/Questions/Accordian.jsx
+++ src/components/Questions/Accordian.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BsArrowDownCircle } from 'react-icons/bs';
 
 function Accordian() {
-	return <div className="accordianContainer"></div>;
+	return (
+		<div className="accordianContainer">
+			<span className="title">
+				Ho do I choose the right travel destination for me?
+				<span>
+					<BsArrowDownCircle className="icon" />
+				</span>
+			</span>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe pariatur
+				ea architecto est laudantium dignissimos totam quae itaque a impedit.
+			</p>
+		</div>
+	);
 }
 
 export default Accordian;
